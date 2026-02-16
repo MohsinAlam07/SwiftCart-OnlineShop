325a22169c53b7b37a317dcb8def3dca08323fa4
Md Mohsin Alam <mohsinalam1141@gmail.com>
2026-02-16 18:14:49 +0600

    Treanding added

--- Script/index.js
+++ Script/index.js
@@ -52,7 +52,7 @@ ${p.category}
 </span>
 
 <span class="text-sm text-gray-500 flex items-center gap-1">
-⭐ ${p.rating.rate} (${p.rating.count})
+<span class="text-yellow-400"><i class="fa-solid fa-star"></span></i>${p.rating.rate} (${p.rating.count})
 </span>
 
 </div>
@@ -70,12 +70,12 @@ $${p.price}
 <!-- BUTTONS -->
 <div class="flex gap-2 mt-3">
 
-<button class="flex-1 border rounded-lg py-2 text-sm hover:bg-gray-100">
-👁 Details
+<button onclick="showDetails(${p.id})" class="flex-1 border rounded-lg py-2 text-sm hover:bg-gray-100">
+<i class="fa-solid fa-eye"></i> Details
 </button>
 
 <button class="flex-1 bg-indigo-600 text-white rounded-lg py-2 text-sm hover:bg-indigo-700">
-🛒 Add
+<i class="fa-solid fa-cart-shopping"></i> Add
 </button>
 
 </div>
@@ -96,23 +96,126 @@ container.innerHTML="";
 // add ALL button
 const allBtn=document.createElement("button");
 allBtn.innerText="All";
-allBtn.className="btn rounded-2xl";
-allBtn.onclick=()=>loadProducts("all");
+allBtn.className="cat-btn btn rounded-2xl";
+allBtn.onclick=(e)=>{ setActive(e.target); loadProducts("all"); };
 container.appendChild(allBtn);
+
 
 categories.forEach(category => {
 
 const btn=document.createElement("button");
 
 btn.innerText=category;
-btn.className="btn rounded-2xl";
+btn.className="cat-btn btn rounded-2xl";
+btn.onclick=(e)=>{ setActive(e.target); loadProducts(category); };
+// btn.className="btn rounded-2xl";
 
-btn.onclick=()=>loadProducts(category);
+// btn.onclick=()=>loadProducts(category);
 
 container.appendChild(btn);
 
 });
 
 }
+function setActive(btn){
+
+document.querySelectorAll(".cat-btn").forEach(b=>{
+    b.classList.remove("bg-indigo-600","text-white");
+});
+
+btn.classList.add("bg-indigo-600","text-white");
+
+}
+const loadTrending = () => {
+
+fetch("https://fakestoreapi.com/products")
+.then(res=>res.json())
+.then(products=>{
+
+const topRated = products
+.sort((a,b)=>b.rating.rate - a.rating.rate)
+.slice(0,3);
+
+showTrending(topRated);
+
+});
+
+};
+const showTrending = (products)=>{
+
+const container=document.getElementById("trending-container");
+container.innerHTML="";
+
+products.forEach(p=>{
+
+const div=document.createElement("div");
+
+div.className="bg-white rounded-xl shadow p-4 flex flex-col";
+
+div.innerHTML=`
+
+<div class="bg-gray-100 rounded-lg p-6 h-52 flex items-center justify-center">
+<img src="${p.image}" class="h-40 object-contain">
+</div>
+
+<div class="flex justify-between items-center mt-3">
+<span class="text-xs bg-indigo-100 text-indigo-600 px-2 py-1 rounded-full">
+${p.category}
+</span>
+
+<span class="text-sm text-gray-500">
+<span class="text-yellow-400"><i class="fa-solid fa-star"></span></i> ${p.rating.rate} (${p.rating.count})
+</span>
+</div>
+
+<h3 class="font-semibold mt-2 line-clamp-2 min-h-[48px]">
+${p.title}
+</h3>
+
+<p class="font-bold text-lg">$${p.price}</p>
+
+<div class="flex gap-2 mt-3">
+
+<button onclick="showDetails(${p.id})" class="flex-1 border rounded-lg py-2 text-sm hover:bg-gray-100">
+<i class="fa-solid fa-eye"></i> Details
+</button>
+
+
+<button class="flex-1 bg-indigo-600 text-white rounded-lg py-2 text-sm hover:bg-indigo-700">
+<i class="fa-solid fa-cart-shopping"></i> Add
+</button>
+
+</div>
+`;
+
+container.appendChild(div);
+
+});
+
+};
+//modal
+function showDetails(id){
+
+fetch(`https://fakestoreapi.com/products/${id}`)
+.then(res=>res.json())
+.then(product=>{
+
+document.getElementById("modal-title").innerText=product.title;
+document.getElementById("modal-img").src=product.image;
+document.getElementById("modal-desc").innerText=product.description;
+document.getElementById("modal-price").innerText="$"+product.price;
+
+document.getElementById("product-modal").classList.remove("hidden");
+
+});
+
+}
+function closeModal(){
+document.getElementById("product-modal").classList.add("hidden");
+}
+// Home and product
+
+
+loadTrending();
 
 loadCatagories();
